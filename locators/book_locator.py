786c2c7633324ebebd8ed9e66ff150b0401cfffe
Samyak <sam2452344@gmail.com>
2020-01-13 22:58:05 +0530

    recovery

--- locators/book_locator.py
+++
@@ -1,6 +0,0 @@
-
-class BookLocators:
-    LINK = "article.product_pod h3 a"
-    NAME = "article.product_pod h3 a"
-    PRICE = "article.product_pod p.price_color"
-    RATING = "article.product_pod p.star-rating"
